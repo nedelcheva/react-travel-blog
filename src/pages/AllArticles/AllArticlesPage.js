5cf05db6d4e5c1d69267ae042f0025b5202454a0
nedelcheva <velimiraaa@icloud.com>
2021-12-14 20:00:01 +0100

    responsiveness

--- src/pages/AllArticles/AllArticlesPage.js
+++ src/pages/AllArticles/AllArticlesPage.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import './allArticlesPage.css'
-import HomePosts from '../../components/HomePosts/HomePosts'
+import Post from '../../components/Post/Post'
 import Sidebar from '../../components/Sidebar/Sidebar'
 
 
 export default function AllArticlesPage() {
     return (
         <div className="all-p">
-            <HomePosts/>
+            <Post/>
             <Sidebar/>
         </div>
     )
